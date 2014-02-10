9d1107d1f5c2862464457d5d876e0be6087b3d0a
Andrew Parelius <parelius@uw.edu>
2014-02-10 08:32:32 -0500

    fixed top

--- src/harlequinmettle/financialsnet/DataPointGraphic.java
+++ src/harlequinmettle/financialsnet/DataPointGraphic.java
@@ -20,18 +20,18 @@ public class DataPointGraphic {
 	public static final int PIXELS_TOP = 30;
 	public static final int PIXELS_BORDER = 3;
 	public static final int PIXELS_HEIGHT = 150;
-	public static Color COLOR_TIME_LINE = Color.orange; // new Color( );
-	public static Color COLOR_HISTOGRAM_BAR = Color.gray; // new Color( 1,1,1 );
-	public static Color COLOR_LABEL_TEXT = Color.white;// new Color( 1,1,1 );
+	public static Color COLOR_HISTOGRAM_BAR_THIS =  new Color( 200,200,200,150);
+	public static Color COLOR_HISTOGRAM_BAR =  new Color( 100,100,250,150 );
+	public static Color COLOR_HISTOGRAM_BAR_VOL =  new Color(255, 195, 30,150);
 	// previously histos
 	private ArrayList<Rectangle2D.Float> bars;
 	private Rectangle2D.Float border;
-	private Point2D.Float minMaxLine = new Point2D.Float (0,0);
-	private Point2D.Float minMaxBars= new Point2D.Float (0,0);
-	private Point2D.Float minMaxHisto = new Point2D.Float (0,0);
+	private Point2D.Float minMaxLine = new Point2D.Float(0, 0);
+	private Point2D.Float minMaxBars = new Point2D.Float(0, 0);
+	private Point2D.Float minMaxHisto = new Point2D.Float(0, 0);
 	private GeneralPath timePath;
 	private String category = "";
-	private String ticker = ""; 
+	private String ticker = "";
 	private int heightFactor = 1;
 	private int rank = 0;
 	private int id = 0;
@@ -42,7 +42,8 @@ public class DataPointGraphic {
 			/ StatInfo.nbars;
 	private boolean general = true;
 	ArrayList<float[]> timeSeriesCompayData = new ArrayList<float[]>();
-	float top,left;
+	float top, left;
+
 	// TreeMap<Float, float[]> technicals;
 
 	public DataPointGraphic(String category, String ticker) {
@@ -60,57 +61,64 @@ public class DataPointGraphic {
 		this.category = category2;
 		this.ticker = ticker2;
 
-		id  =  Database.dbSet.indexOf(ticker);
+		id = Database.dbSet.indexOf(ticker);
 		List list = Arrays.asList(DBLabels.priorityLabeling);
 		this.rank = list.indexOf(category2);
-	 
+
 		list = Arrays.asList(DBLabels.labels);
 		this.categoryId = list.indexOf(category2);
-	 
+
 		if (general) {
-			graphicRank += heightFactor * (rank + 1);
+			graphicRank += heightFactor * (rank );
 			setUpGeneralGraphData();
 		} else {
-			graphicRank += heightFactor * (  1);
+			graphicRank += heightFactor * (1);
 			setUpTechnicalsGraphData();
 		}
+		top = PIXELS_BORDER + graphicRank * PIXELS_BORDER + graphicRank
+				* PIXELS_HEIGHT;
+		left = PIXELS_BORDER;
 
-		border = new Rectangle2D.Float(PIXELS_BORDER, PIXELS_BORDER + rank
-				* PIXELS_BORDER + rank * PIXELS_HEIGHT, ProfileCanvas.W - 2
-				* PIXELS_BORDER, PIXELS_HEIGHT * heightFactor);
+		border = new Rectangle2D.Float(PIXELS_BORDER, PIXELS_BORDER
+				+ graphicRank * PIXELS_BORDER + graphicRank * PIXELS_HEIGHT,
+				ProfileCanvas.W - 2 * PIXELS_BORDER, PIXELS_HEIGHT
+						* heightFactor);
 	}
 
 	private void setMinMaxHistogramHighlight() {
- 
+
 		ArrayList<Float> variations = new ArrayList<Float>();
-		for(float[] timeSeries: timeSeriesCompayData	){
-			variations.add(timeSeries[categoryId]	);
-		} 
+		for (float[] timeSeries : timeSeriesCompayData) {
+			variations.add(timeSeries[categoryId]);
+		}
 		float min = min(variations);
 		float max = max(variations);
 
 		if (min == min)
-			minMaxHisto.x =	Database.statistics.get(categoryId).locationInHistogram(min);
+			minMaxHisto.x = Database.statistics.get(categoryId)
+					.locationInHistogram(min);
 		else
 			minMaxHisto.x = -10;
 
 		if (max == max)
-			minMaxHisto.y =	Database.statistics.get(categoryId).locationInHistogram(max);
+			minMaxHisto.y = Database.statistics.get(categoryId)
+					.locationInHistogram(max);
 		else
 			minMaxHisto.y = -10;
-		 
+
 	}
 
 	private void setUpTechnicalsGraphData() {
 
-		TreeMap<Float, float[]> technicals  = Database.TECHNICAL_PRICE_DATA.get(Database.dbSet.get(id));
+		TreeMap<Float, float[]> technicals = Database.TECHNICAL_PRICE_DATA
+				.get(Database.dbSet.get(id));
 		ArrayList<Point2D.Float> marketToStockPairing = pairPriceWithMarketByDate(
 				technicals, 6);
 		ArrayList<Float> stockPrices = makeListFromPricePair(
 				marketToStockPairing, 0);
 		bars = createVolumeBars(technicals);
-		
-timePath = new GeneralPath();
+
+		timePath = new GeneralPath();
 	}
 
 	private void setUpGeneralGraphData() {
@@ -310,53 +318,56 @@ timePath = new GeneralPath();
 	}
 
 	public void rescale() {
-		//init(category, ticker);
+		graphicRank = 0;
+		// init(category, ticker);
 	}
-public void drawMe(Graphics2D g){
- 
- int J = 0;
+
+	public void drawMe(Graphics2D g) {
+
+		int J = 0;
 		for (Rectangle2D.Float bar : bars) {
-			if(general){
-			if (minMaxHisto.x ==  J  || minMaxHisto.y ==J   )
-				g.setColor(Color.white);
-			else 
-				g.setColor(new Color(20, 20, 10 + 10 * 11));
-			} else{ 
-				g.setColor(Color.orange); 
+			if (general) {
+				if (minMaxHisto.x == J || minMaxHisto.y == J)
+					g.setColor(COLOR_HISTOGRAM_BAR_THIS);
+				else
+					g.setColor(COLOR_HISTOGRAM_BAR);
+			} else {
+				g.setColor(COLOR_HISTOGRAM_BAR_VOL);
 			}
-			g.fill(bar); 
+			g.draw(bar);
+			  g.fill(bar);
 			J++;
-	}  
+		}
 		g.setColor(new Color(20, 20, 10 + 10 * 19));
 		g.draw(border);
- 
-			g.setColor(Color.white);
-			g.drawString(category, left+PIXELS_BORDER, top+15);
 
-			g.drawString("" +minMaxLine.x, ProfileCanvas.W - 100, top+PIXELS_HEIGHT-PIXELS_BORDER);
-			g.drawString("" + minMaxLine.y, ProfileCanvas.W - 100, top + 13);
-	  
-if(general)
-	g.setColor(Color.blue);
-else
-	g.setColor(Color.magenta);
-	g.draw(timePath);
-drawHighlightWindows(g);
-	
- 
-}
+		g.setColor(Color.white);
+		g.drawString(category, left + PIXELS_BORDER, top + 15);
 
-private void drawHighlightWindows(Graphics2D g) {
+		g.drawString("" + minMaxLine.x, ProfileCanvas.W - 100, top
+				+ PIXELS_HEIGHT - PIXELS_BORDER);
+		g.drawString("" + minMaxLine.y, ProfileCanvas.W - 100, top + 13);
 
-//	g.setColor(new Color(200, 200, 200, 200));
-//	g.fillRect(W - 110, H - 70, 90, 40);
-//	g.fillRect(W - 110, H - 4 * PART - 0, 90, 40);
-//	g.setColor(new Color(205, 00, 205));
-//	g.drawString("" + priceRange.x, W - 100, H - 55);
-//	g.drawString("" + priceRange.y, W - 100, H - 4 * PART + 15);
-//
-//	g.setColor(new Color(205, 100, 100));
-//	g.drawString("" + volRange.x, W - 100, H - 40);
-//	g.drawString("" + volRange.y, W - 100, H - 4 * PART + 30);
-}
+		if (general)
+			g.setColor(Color.blue);
+		else
+			g.setColor(Color.magenta);
+		g.draw(timePath);
+		drawHighlightWindows(g);
+
+	}
+
+	private void drawHighlightWindows(Graphics2D g) {
+
+		// g.setColor(new Color(200, 200, 200, 200));
+		// g.fillRect(W - 110, H - 70, 90, 40);
+		// g.fillRect(W - 110, H - 4 * PART - 0, 90, 40);
+		// g.setColor(new Color(205, 00, 205));
+		// g.drawString("" + priceRange.x, W - 100, H - 55);
+		// g.drawString("" + priceRange.y, W - 100, H - 4 * PART + 15);
+		//
+		// g.setColor(new Color(205, 100, 100));
+		// g.drawString("" + volRange.x, W - 100, H - 40);
+		// g.drawString("" + volRange.y, W - 100, H - 4 * PART + 30);
+	}
 }
